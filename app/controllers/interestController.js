d75ad83547433d1ebd1376ebed8fdac0e1537dd8
Karolina <karolina.limanowska@gmail.com>
2024-10-31 17:22:16 +0100

    fetching and displaying all user info on the users account page

--- app/controllers/interestController.js
+++ app/controllers/interestController.js
@@ -1,12 +1,12 @@
 import { supabase } from "@/db/dbConnection";
 
 export const Interests = {
-    addUserInterests: async (user_id, interests) => {
+    addUserInterests: async (userId, interests) => {
 
         //formatting interests as an array of objects
         const interestsData = interests.map(interest => ({
-            user_id,
-            interest_id: interest,
+            userId,
+            interestId: interest,
         }));
 
         const { data, error } = await supabase
@@ -18,5 +18,31 @@ export const Interests = {
             return { success: false, error };
         }
         return { success: true, data}
+     },
+
+     readUserInterests: async (userId) => {
+        const { data, error } = await supabase
+        .from('Users_Interests')
+        .select('*')
+        .eq('user_id', userId)
+
+        if (error) {
+            console.error('Error fetching interests for this user ID:', error.message);
+            return { success: false, error };
+        }
+        return { success: true, data }
+     },
+
+     readInterestsByIds: async (interestIds) => {
+        const { data, error } = await supabase
+        .from ('Interests')
+        .select ('interest')
+        .in('id', interestIds);
+
+        if (error) {
+            console.error('Error fetching interests by Ids');
+            return { success: false, error}
+        }
+        return { success: true, data }
      }
 };
--- app/user-account/page.js
+++ app/user-account/page.js
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react"
 import { Users } from "../controllers/userController"
+import { Interests } from "../controllers/interestController"
 
 export default function UserProfile() {
 
@@ -12,24 +13,40 @@ export default function UserProfile() {
         gender: "",
         traveler_type: "",
         interests: []
-    })
+    });
+
+    const [userInterests, setUserInterests] = useState([]);
     
     useEffect(() => {
-        const fetchUserData = async () => {
+        const fetchUserDataAndInterests = async () => {
             const userId = localStorage.getItem('user_id');
             if (!userId) {
                 console.error('No user ID found in the local storage');
                 return
             }
 
-            const response = await Users.readUserById(userId);
-            if (response.success) {
-                setUserData(response.data);
+            const userResponse = await Users.readUserById(userId);
+            if (userResponse.success) {
+                setUserData(userResponse.data);
             } else {
-                console.error('Failed to fetch user:', response.error)
+                console.error('Failed to fetch user:', userResponse.error)
+            }
+
+            const interestsResponse = await Interests.readUserInterests(userId);
+            if (interestsResponse.success) {
+                const interestId = interestsResponse.data.map((item) => item.interest_id);
+
+                const interestNameResponse = await Interests.readInterestsByIds(interestId);
+                if (interestNameResponse.success) {
+                    setUserInterests(interestNameResponse.data.map((item) => item.interest))
+                } else {
+                    console.error('Failed to fetch interest name:', interestNameResponse.error);
+                }
+            } else {
+                console.error('Failed to fetch user interests:', interestsResponse.error)
             }
         };
-        fetchUserData();
+        fetchUserDataAndInterests();
     }, []);
        
     
@@ -42,6 +59,7 @@ export default function UserProfile() {
             <div>Age Group: {userData.age_group}</div>
             <div>Gender: {userData.gender}</div>
             <div>Traveler Type: {userData.traveler_type}</div>
+            <div>Interests: {userInterests.length > 0 ? userInterests.join(", ") : "None"}</div>
             
         </div>
     )
